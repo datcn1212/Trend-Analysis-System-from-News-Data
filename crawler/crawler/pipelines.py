456ad7fa473a73713536a74bdde493a26fb0cb10
datcn1212 <datcn.12.2001@gmail.com>
2023-05-14 23:30:35 +0700

    final save to hdfs with pyspark

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -9,20 +9,40 @@ from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 from scrapy.exporters import JsonItemExporter
 import json
-from hdfs import InsecureClient
+from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql import SparkSession
 
 class CrawlerPipeline:
 
+    def __init__(self):
+        self.spark = SparkSession.builder \
+                .appName("Write to HDFS") \
+                .getOrCreate()
+        self.hdfs_path = "hdfs://localhost:9000/user/root/test1.parquet"
+        self.lst_data = []
+        self.schema = StructType([
+                        StructField("Topic", StringType(), nullable=False),
+                        StructField("Date", StringType(), nullable=True),
+                        StructField("Author", StringType(), nullable=True),
+                        StructField("Title", StringType(), nullable=False),
+                        StructField("Href", StringType(), nullable=False),
+                        StructField("Description", StringType(), nullable=True),
+                        StructField("Body", StringType(), nullable=False)
+                    ])
+
     def open_spider(self, spider):
         self.file = open("data1.json", "w", encoding='utf-8')
         self.file.write('[\n')
-        # HDFS client
-        # self.client = InsecureClient('http://localhost:9000', user='root')
 
     def close_spider(self, spider):
         self.file.write('{}]')
         self.file.close()
+        # save final dataframe to HDFS
+        df = self.spark.createDataFrame(self.lst_data, self.schema)
+        df.write.json(self.hdfs_path)
+
+        # stop session
+        self.spark.stop()
 
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
@@ -31,28 +51,10 @@ class CrawlerPipeline:
             raise DropItem(f"Missing author in {item}")
         else:
             # Save to file
-            line = json.dumps(ItemAdapter(item).asdict(), ensure_ascii=False) + ",\n"
-            self.file.write(line)
+            item_line = json.dumps(ItemAdapter(item).asdict(), ensure_ascii=False) 
+            self.file.write(item_line+ ",\n")
 
-            # Save to HDFS
-
-            spark = SparkSession.builder \
-                .appName("Write to HDFS") \
-                .getOrCreate()
-            data = [("Alice", 25), ("Bob", 30), ("Charlie", 35)]
-            df = spark.createDataFrame(data, ["Name", "Age"])
-            hdfs_path = "hdfs://localhost:9000/user/root/data.parquet"
-            df.write.parquet(hdfs_path)
-
-            # hdfs_output_path = '/user/caodat/file.json'
-            # with self.client.write(hdfs_output_path, overwrite=True) as file:
-            #     file.write(json.dumps(ItemAdapter(item).asdict(), ensure_ascii=False))
-
+            # Union new item to dataframe
+            i = ItemAdapter(item).asdict()
+            self.lst_data.append(i)
             return item
-        
-        # return item
-    # def open_spider(self, spider):
-    #     self.filename = "x.json"
-    #     self.jsonfile  = open(self.filename, 'wb')
-    #     self.exporter = JsonItemExporter(self.jsonfile, encoding='utf-8')
-    #     self.exporter.start_exporting()
