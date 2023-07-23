2b03e6938a34338a698e055c8e8a76ac617e1802
datcn1212 <datcn.12.2001@gmail.com>
2023-07-23 15:43:10 +0700

    dags

--- air_flow/dags/run_process.py
+++ air_flow/dags/run_process.py
@@ -1,19 +1,17 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 
-# Define the DAG
 dag = DAG(
-    'bash_script',
+    'run_process',
     description='DAG to run a Bash script',
-    start_date=datetime(2023, 6, 18),
-    schedule_interval=None
+    start_date=datetime(2023, 7, 23),
+    schedule_interval='50 7 * * *', 
+    catchup=False, 
 )
 
 run_bash_script = BashOperator(
     task_id='run_bash_script',
-    bash_command='pwd && cd ../crawler && scrapy crawl crawler2',
+    bash_command='cd /home/canada/DATN_CND/DATN && ',
     dag=dag
 )
-
-run_bash_script
---
+++ air_flow/dags/run_vnexpress.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+dag = DAG(
+    'run_vnexpress',
+    description='DAG to run a Bash script',
+    start_date=datetime(2023, 7, 23),
+    schedule_interval='30 0 * * *', 
+    catchup=False,  
+)
+
+run_bash_script = BashOperator(
+    task_id='run_bash_script',
+    bash_command='cd /home/canada/DATN_CND/DATN/crawler && scrapy crawl vnexpress_main --logfile log_vnex.txt',
+    dag=dag
+)
